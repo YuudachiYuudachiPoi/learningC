bc4875a05f5e5904d369c90dd875bc4bfdbd338c
YuudachiYuudachiPoi <mcgogogo@qq.com>
2019-03-22 13:11:51 -0400

    start

---
+++ hellow_world.cpp
@@ -0,0 +1,15 @@
+/*
+*Author: Howie Hong(希理)
+*LastEditors: Howie Hong(希理)
+*Description: 
+*Date: 2019-03-22 12:53:36
+*LastEditTime: 2019-03-22 13:02:12
+*/
+#include  <iostream>
+using namespace std;
+
+int main()
+{   
+       cout << "Hello Worgershgrgsrsr\n";
+       return 0;
+}
